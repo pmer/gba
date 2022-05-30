ab0798fa4394d222e450e73a5a1a0239267e576f
Paul Merrill <pdm@pdm.me>
2022-05-30 13:54:56 -0700

    main: Draw lines around rectangle

--- src/main.cpp
+++ src/main.cpp
@@ -32,9 +32,70 @@ sing() noexcept {
 }
 
 static void
-drawPattern() noexcept {
+drawRect(s32 left, s32 top, s32 width, s32 height, COLOR color) noexcept {
+    s32 right = left + width;
+    s32 bottom = top + height;
+    for (s32 y = top; y < bottom; y++) {
+        for (s32 x = left; x < right; x++) {
+            m3_mem[y][x] = color;
+        }
+    }
+}
+
+static s32
+abs(s32 i) noexcept {
+    s32 mask = i >> 31;
+    return (i ^ mask) - mask;
+}
+
+static void
+drawLine(s32 x, s32 y, s32 x2, s32 y2, COLOR color) noexcept {
+    s32 w = x2 - x;
+    s32 h = y2 - y;
+    s32 dx1 = 0, dx2 = 0, dy1 = 0, dy2 = 0;
+
+    if (w < 0) dx1 = dx2 = -1;
+    else if (w > 0) dx1 = dx2 = 1;
+    if (h < 0) dy1 = -1;
+    else if (h > 0) dy1 = 1;
+
+    s32 longest = abs(w);
+    s32 shortest = abs(h);
+    if (shortest > longest) {
+        shortest ^= longest;
+        longest ^= shortest;
+        shortest ^= longest;
+
+        if (h < 0) dy2 = -1;
+        else if (h > 0) dy2 = 1;
+        dx2 = 0;
+    }
+
+    s32 numerator = longest >> 1;
+    for (s32 i = 0; i <= longest; i++) {
+        m3_mem[y][x] = color;
+        numerator += shortest;
+        if (numerator > longest) {
+            numerator -= longest;
+            x += dx1;
+            y += dy1;
+        }
+        else {
+            x += dx2;
+            y += dy2;
+        }
+    }
+}
+
+static void
+mode3() noexcept {
     // Set GBA rendering context to mode 3 with bitmap rendering.
     REG_DISPCNT = DCNT_MODE3 | DCNT_BG2;
+}
+
+static void
+drawPattern() noexcept {
+    mode3();
 
     int t = 0;
 #pragma clang diagnostic push
@@ -67,6 +128,17 @@ main() noexcept {
     printf_("Hello, world!");
 
     //REG_DISPCNT = DCNT_MODE0 | DCNT_BG0;
+
+    mode3();
+
+    COLOR salmon = RGB15(31, 5, 12);
+    drawRect(20, 20, SCREEN_WIDTH - 40, SCREEN_HEIGHT - 40, salmon);
+
+    COLOR green = RGB15(1, 24, 16);
+    drawLine(20-1, 20-1, 0-1, 0-1, green); // Top-left.
+    drawLine(220, 20-1, 240, 0-1, green); // Top-right.
+    drawLine(20-1, 140, 0-1, 160, green); // Bottom-left.
+    drawLine(220, 140, 240, 160, green); // Bottom-right.
 
     irq_init(nullptr);
     irq_add(II_VBLANK, nullptr);
